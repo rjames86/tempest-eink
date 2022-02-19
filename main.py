78a600808c1495c376d5c034660d3250fa3b2cec
Ryan M <ryan@ryanmo.co>
2022-02-19 08:45:50 -0700

    getting test chart to appear

---
+++ main.py
@@ -0,0 +1,25 @@
+from PIL import Image, ImageOps
+from PIL import ImageFont
+from PIL import ImageDraw
+
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from datetime import timedelta
+
+from tempest.observations import get_observations
+
+observations = get_observations("obs_st")
+
+dates = [obs.timestamp for obs in observations]
+temps = [obs.air_temperature for obs in observations]
+
+fig, ax = plt.subplots()
+ax.plot(dates, temps)
+ax.xaxis_date()
+
+formatter = mdates.DateFormatter("%b %d %H:%M")
+ax.xaxis.set_major_formatter(formatter)
+
+fig.autofmt_xdate()
+
+plt.show()
