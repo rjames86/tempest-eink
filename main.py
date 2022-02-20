d55caa83039814ddd55a06667992364a8560c948
Ryan M <ryan@ryanmo.co>
2022-02-20 12:01:04 -0700

    run continuously

--- main.py
+++ main.py
@@ -32,13 +32,6 @@ if not testing:
 else:
     epd = MockEPD()
 
-forecast = get_forecast()
-observations = get_observations()
-
-
-epd.init()
-epd.Clear()
-
 font12 = ImageFont.truetype("./fonts/Font.ttc", 12)
 font24 = ImageFont.truetype("./fonts/Font.ttc", 24)
 font18 = ImageFont.truetype("./fonts/Font.ttc", 18)
@@ -46,51 +39,62 @@ font36 = ImageFont.truetype("./fonts/Font.ttc", 36)
 font48 = ImageFont.truetype("./fonts/Font.ttc", 48)
 icon_font = ImageFont.truetype("./fonts/meteocons.ttf", 48)
 
-# Drawing on the Horizontal image
-Himage = Image.new("1", (epd.width, epd.height), 255)  # 255: clear the frame
-draw = ImageDraw.Draw(Himage)
 
-# # Create the bounding box for current conditions
-side_padding = 5
-top_padding = 30
-x0, y0 = (0 + side_padding), (0 + top_padding)
-x1, y1 = (epd.width - side_padding), ((epd.height - top_padding) * 2 // 3)
+def main():
+    forecast = get_forecast()
+    observations = get_observations()
 
-full_rect = [x0, y0, x1, y1]
-half_rect = [x0, y0, x1 // 2, y1]
-quarter_rect = [x0, y0, x1 // 4, y1]
+    epd.init()
+    # Drawing on the Horizontal image
+    Himage = Image.new("1", (epd.width, epd.height), 255)  # 255: clear the frame
+    draw = ImageDraw.Draw(Himage)
 
-# We create the charts before anything else. Since there's
-# some weird padding issues, I want the rectangle to draw
-# over the images
-charts = Charts(Himage, observations, 0, y1)
-charts.create()
+    # # Create the bounding box for current conditions
+    side_padding = 5
+    top_padding = 30
+    x0, y0 = (0 + side_padding), (0 + top_padding)
+    x1, y1 = (epd.width - side_padding), ((epd.height - top_padding) * 2 // 3)
 
-now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-draw.text((5, 5), "Last updated: %s" % now, font=font12, fill=0)
-draw.rectangle(full_rect, fill=255, outline=0)
+    full_rect = [x0, y0, x1, y1]
+    half_rect = [x0, y0, x1 // 2, y1]
+    quarter_rect = [x0, y0, x1 // 4, y1]
 
-c = CurrentConditions(
-    Himage,
-    forecast,
-    observations,
-    full_rect,
-)
-c.create()
+    # We create the charts before anything else. Since there's
+    # some weird padding issues, I want the rectangle to draw
+    # over the images
+    charts = Charts(Himage, observations, 0, y1)
+    charts.create()
 
-f = Forecasts(
-    Himage,
-    forecast,
-    observations,
-    full_rect,
-    top_padding,
-)
-f.create()
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    draw.text((5, 5), "Last updated: %s" % now, font=font12, fill=0)
+    draw.rectangle(full_rect, fill=255, outline=0)
 
-if not testing:
-    epd.display(epd.getbuffer(Himage))
+    c = CurrentConditions(
+        Himage,
+        forecast,
+        observations,
+        full_rect,
+    )
+    c.create()
 
-    time.sleep(60 * 3)
-    epd.Clear()
-else:
-    Himage.show()
+    f = Forecasts(
+        Himage,
+        forecast,
+        observations,
+        full_rect,
+        top_padding,
+    )
+    f.create()
+
+    if not testing:
+        if datetime.now().strftime("%M") == "00":
+            print("Clearing screen to avoid burn in")
+            epd.Clear()
+
+        epd.display(epd.getbuffer(Himage))
+        time.sleep(10)
+        epd.sleep()
+        print("sleeping for 2 minutes")
+        time.sleep(60 * 2)
+    else:
+        Himage.show()
